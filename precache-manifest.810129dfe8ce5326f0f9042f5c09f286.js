3394036db10b4d57baca22df1a37934d9738b2f2
kaarenzu <karenzcortes0211@gmail.com>
2020-04-17 02:34:07 -0400

    Updates

---
+++ precache-manifest.810129dfe8ce5326f0f9042f5c09f286.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de69c81b2e92579e0f4076323647d28d",
+    "url": "/Portafolio/%20./index.html"
+  },
+  {
+    "revision": "30fd85deae21f65c228d",
+    "url": "/Portafolio/%20./static/css/main.0671bf51.chunk.css"
+  },
+  {
+    "revision": "817327a66d7cd0a59952",
+    "url": "/Portafolio/%20./static/js/2.19f8ac03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portafolio/%20./static/js/2.19f8ac03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fd85deae21f65c228d",
+    "url": "/Portafolio/%20./static/js/main.f87376d6.chunk.js"
+  },
+  {
+    "revision": "ec8a4d87a643b58c3db1",
+    "url": "/Portafolio/%20./static/js/runtime-main.91f0bf46.js"
+  },
+  {
+    "revision": "bc69ed779d013456723be26942f89c46",
+    "url": "/Portafolio/%20./static/media/Linkedin.bc69ed77.ico"
+  },
+  {
+    "revision": "b71c751f9abf18ef5d3e66fddac03765",
+    "url": "/Portafolio/%20./static/media/css3.b71c751f.ico"
+  },
+  {
+    "revision": "3839754e60784dba6dd7f6119ea70c8b",
+    "url": "/Portafolio/%20./static/media/git.3839754e.ico"
+  },
+  {
+    "revision": "b63e086c19720586360ed135048e5807",
+    "url": "/Portafolio/%20./static/media/github.b63e086c.ico"
+  },
+  {
+    "revision": "4b73ffa1abe634f1f3b385b3e7ec312f",
+    "url": "/Portafolio/%20./static/media/gmail.4b73ffa1.ico"
+  },
+  {
+    "revision": "dee467d4329f765b4ac1b8277c65dd90",
+    "url": "/Portafolio/%20./static/media/html5.dee467d4.ico"
+  },
+  {
+    "revision": "8ddfe59eac00e7bb0c116a1a1296ac55",
+    "url": "/Portafolio/%20./static/media/javascript.8ddfe59e.ico"
+  },
+  {
+    "revision": "7fd35780fea1b7948b4e9d5a4283e04b",
+    "url": "/Portafolio/%20./static/media/react.7fd35780.ico"
+  }
+]);
