39b379a4f1e66098de6aee113e103cde38713185
kaarenzu <karenzcortes0211@gmail.com>
2020-04-17 12:15:42 -0400

    Cambie link de imagenes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import { Button } from 'reactstrap';
 import MyNavbar from './Component/Navbar.jsx'
-// import { MyContainer } from './Component/MyContainer'
 import {
   BrowserRouter as Router
 } from "react-router-dom";
-
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div className="app">
-
           <MyNavbar />
-
-
         </div>
       </Router>
-
     );
   }
 }
-
 export default App;
