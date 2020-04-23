4b4ae309aed2b1d2533bacf6c8f48e2142fa5c7b
kaarenzu <karenzcortes0211@gmail.com>
2020-04-23 16:08:54 -0400

    Agregué link de proyectos y modifiqé el css para responsive

--- src/Component/Proyectos.jsx
+++ src/Component/Proyectos.jsx
@@ -1,10 +1,57 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import '../Css/proyectos.css'
 
-function Proyectos(){
-  return(
-    <div>
-      <h1>Pendiente...</h1>
+function Proyectos() {
+  return (
+    <div className="divProyectos">
+      <div classsName="containerProyecto" id="yapo">
+        <h2 className="classh1">Mis Proyectos</h2>
+        <div className="cajaproyectos">
+          <a href={`https://here-squad2.firebaseapp.com/`}
+            target="_blank">
+            <img
+              src={require("../imgProyectos/loginHere.png")}
+              className="imgProyectos" />
+            <div className="descripcion">
+              <p>En Move Calm planifica tus viajes con la seguridad de no estar solx, podrás
+            encontrar una comunidad con la cual interactuar, videos de relajación y meditación.</p>
+            </div>
+          </a>
+        </div>
+        <div className="cajaproyectos">
+          <a href={`https://bdiniscia.github.io/SCL012-MovieProject/src/`}
+            target="_blank" >
+            <img
+              src={require("../imgProyectos/movieProyect.png")}
+              className="imgProyectos" />
+            <div className="descripcion">
+              <p>Cine La Costa está sorteando dos entradas a quien participe y gane una
+              entretenida trivia con preguntas acerca de las peliculas que están nominada a los
+              Oscar 2020.Participa y gana.</p>
+            </div>
+          </a>
+
+        </div>
+        <div className="cajaproyectos">
+          <a href={`https://tipstech-c6fe7.firebaseapp.com/#/Inicio`}
+            target="_blank" >
+            <img
+              src={require("../imgProyectos/loginTips.png")}
+              className="imgProyectos" />
+            <div className="descripcion">
+              <p>En Tips Tech podrás encontrar y compartir datos de ofertas en productos
+              tecnologicos y así poder comprar los mejores productos a precios muy convenientes.
+            </p>
+            </div>
+          </a>
+        </div>
+
+      </div>
     </div>
+
   )
 }
 
